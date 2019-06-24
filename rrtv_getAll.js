24d55b2754db5a02e73f94ac95699a6d1b3f93b9
Asaki <37176460+yudingsxt@users.noreply.github.com>
2019-06-24 19:27:44 +0800

    Update rrtv_getAll.js

--- rrtv_getAll.js
+++ rrtv_getAll.js
@@ -2,7 +2,7 @@ var body = $response.body;
   var obj = JSON.parse(body);
   obj.data.medalList.forEach((element, index) => {
     if (element.medal.id === 1) {
-      obj.data.medalList[index].medal.endTime = "2022-01-01 00:00:00";
+      obj.data.medalList[index].medal.endTime = "2025-05-20 00:00:00";
     }
   });
   body = JSON.stringify(obj);
