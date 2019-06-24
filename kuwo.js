6be334966aad4d8699f8addf665abb945763448e
Asaki <37176460+yudingsxt@users.noreply.github.com>
2019-06-24 18:46:03 +0800

    Create kuwo.js

---
+++ kuwo.js
@@ -0,0 +1,11 @@
+var body=$response.body;
+    let jsbody = JSON.parse(body);
+    jsbody.data.isNewUser= 2;
+    jsbody.data.vipLuxuryExpire= 1738717749000;
+    jsbody.data.isYearUser= 2;
+    jsbody.data.vipmExpire= 1738717749000;
+    jsbody.datavipOverSeasExpire= 1738717749000;
+    jsbody.data.vipExpire= 1738717749000;
+    jsbody.data.vip3Expire= 1738717749000;
+    body = JSON.stringify(jsbody);
+$done(body);
