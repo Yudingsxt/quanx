2410b148cf7744fcf08193e1200e1a76b1fb8444
Asaki <37176460+yudingsxt@users.noreply.github.com>
2019-06-26 00:10:05 +0800

    Create feed.js

---
+++ feed.js
@@ -0,0 +1,9 @@
+let body = $response.body;
+obj=JSON.parse(body);
+obj['data'].forEach((element, index)=>{
+     if(element.hasOwnProperty('ad')){      
+       body['data'].splice(index,1);  
+    }
+})
+body=JSON.stringify(obj);
+$done({body});
