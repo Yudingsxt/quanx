ff1d55991c149a95dc0c8fe777fa1a651db99649
Asaki <37176460+yudingsxt@users.noreply.github.com>
2019-06-26 00:07:01 +0800

    Create recommend.js

---
+++ recommend.js
@@ -0,0 +1,9 @@
+let body = $response.body;
+obj=JSON.parse(body);
+obj['data'].forEach((element, index)=> {
+    if(element['card_type']=="slot_event_card"||element.hasOwnProperty('ad')){      
+       obj['data'].splice(index,1);  
+    }
+})
+body=JSON.stringify(obj);
+$done({body});
