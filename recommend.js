47e961214d292eb85d96a8a429d46b0866ad9b04
Asaki <37176460+yudingsxt@users.noreply.github.com>
2019-06-26 07:59:51 +0800

    Update recommend.js

--- recommend.js
+++ recommend.js
@@ -4,6 +4,6 @@ obj['data'].forEach((element, index)=> {
     if(element['card_type']=="slot_event_card"||element.hasOwnProperty('ad')){      
        obj['data'].splice(index,1);  
     }
-})
+});
 body=JSON.stringify(obj);
 $done(body);
