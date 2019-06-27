82baf2e03624cd0660abbc37d4460a5afc4d30e9
Asaki <37176460+yudingsxt@users.noreply.github.com>
2019-06-27 20:33:01 +0800

    Update weread.js

--- weread.js
+++ weread.js
@@ -3,6 +3,7 @@ var body = $response.body;
   let obj = JSON.parse(body);
   obj.expiredTime= 1747624048;
   obj.remainTime= 189302400;
+  obj.expired= 0;
   body = JSON.stringify(obj);
 
 $done(body);
