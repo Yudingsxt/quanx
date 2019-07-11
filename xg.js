164a205a4a3eab4158cf9a3c74945598b90c5f51
Asaki <37176460+yudingsxt@users.noreply.github.com>
2019-07-11 11:36:38 +0800

    Update xg.js

--- xg.js
+++ xg.js
@@ -1,12 +1,10 @@
 var body = $response.body;
 var url = $request.url;
-if (url.indexOf(path1) != -1){
   let obj = JSON.parse(body);
   obj.content["nickName"] = "Meeta_share";
   obj.content["level"] = "5";
   obj.content["vipType"] = "3";
   obj.content["expirationTime"] = "3029548557000";
   body = JSON.stringify(obj);
-}
 
 $done({body});
